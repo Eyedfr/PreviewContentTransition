3531d4ce3ac49d389d3db1e6475bb99da0f5abc8
Eyedfr <e.friaa@milkdesign.de>
2023-03-29 11:34:31 +0200

    Deleted cursor

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,4 @@
 import { preloadImages, preloadFonts } from './utils';
-import { Cursor } from './cursor';
 import { Item }  from './item';
 
 // Preload images and fonts
@@ -7,15 +6,10 @@ Promise.all([preloadImages('.item__img, .content__img'), preloadFonts('ytb6dpl')
     // remove loader (loading class) 
     document.body.classList.remove('loading');
     
-    // initialize custom cursor
-    const cursor = new Cursor(document.querySelector('.cursor'));
+    // initialize custom ^
 
     let itemsArr = [];
     [...document.querySelectorAll('.items > .item')].forEach(item => itemsArr.push(new Item(item, itemsArr)));
 
     // mouse effects on all links and others
-    [...document.querySelectorAll('a, .unbutton')].forEach(link => {
-        link.addEventListener('mouseenter', () => cursor.enter());
-        link.addEventListener('mouseleave', () => cursor.leave());
-    });
 });
